8475647cb6458920382bca92d0d9f979a80673e0
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2023-01-16 11:26:18 -0300

    commenting a little

--- src/directives/basic-highlight.directive.ts
+++ src/directives/basic-highlight.directive.ts
@@ -1,7 +1,7 @@
 import {Directive, ElementRef, OnInit} from "@angular/core";
 
 @Directive({
-  selector: '[appBasicHighlight]'
+  selector: '[appBasicHighlight]' // the square brackets indicate that this directive will be a property, if they weren't present, it would indicate that the selector was an element
 })
 
 export class BasicHighlightDirective implements OnInit {
